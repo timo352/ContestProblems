a5a89ccaa22c09fd14f3ae631913fe6532741d75
timo352 <timothyglensmith@cedarville.edu>
2016-12-10 14:24:45 -0500

    Testing commit from NetBeans Modified AllPairsShortestPathNotShortestName

--- src/AllPairsShortestPathNotShortestName.java
+++ src/AllPairsShortestPathNotShortestName.java
@@ -4,9 +4,6 @@ import java.io.*;
 public class AllPairsShortestPathNotShortestName {
 	
 	public static void main(String[] args){
-		
-		
-		
-		
+		System.out.println("This isn't implemented yet!");	
 	}
 }
